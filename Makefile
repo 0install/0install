b98fc54e3c851ca97d92168923197c4b6822fd0a
Thomas <Leonard>
2005-02-04 19:31:53 +0000

    Restructuing.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/injector/head/injector-gui/head@43 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	pychecker *.py
-
-test:
-	./injector/injector-auto injector-gui.xml injector-gui Tests/edit.xml AppRun
--- injector/namespaces.py
+++
@@ -1,4 +0,0 @@
-XMLNS_IFACE = 'http://zero-install.sourceforge.net/2004/injector/interface'
-
-config_site = '0install.net'
-config_prog = 'injector'
