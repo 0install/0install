cf2e9d2873d601f9ca3da4aa0a8bd8e393dc9fa3
Thomas Leonard <talex5@gmail.com>
2005-03-13 13:23:56 +0000

    Unpack archives to tmp dir.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/injector/head/injector/head@88 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- zerostore/__init__.py
+++ zerostore/__init__.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import traceback
+from tempfile import mkdtemp
 
 class Store:
 	def __init__(self, dir):
@@ -18,3 +21,28 @@ class Store:
 		"""Data is a .tgz compressed archive. Extract it somewhere, check that
 		the digest is correct, and add it to the store."""
 		print "Adding impl with digest:", required_digest
+
+		self.extract(data, ('tar', 'xz'))
+
+	def extract(self, stream, command):
+		tmp = mkdtemp(dir = self.dir, prefix = 'tmp-')
+		try:
+			child = os.fork()
+			if child == 0:
+				try:
+					try:
+						os.chdir(tmp)
+						stream.seek(0)
+						os.dup2(stream.fileno(), 0)
+						os.execvp(command[0], command)
+					except:
+						traceback.print_exc()
+				finally:
+					os._exit(1)
+			id, status = os.waitpid(child, 0)
+			assert id == child
+			if status != 0:
+				raise Exception('Failed to extract archive; exit code %d' % status)
+		except:
+			shutil.rmtree(tmp)
+			raise
