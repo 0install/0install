f7778010ce6e69fff38db25fc01e97f2630156cf
Thomas Leonard <talex5@gmail.com>
2005-05-08 20:33:43 +0000

    Added tests for better coverage.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/injector/head/injector/head@178 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- tests/testgpg.py
+++ tests/testgpg.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2.2
-import sys, tempfile
+import sys, tempfile, os, shutil
 import unittest
 
 sys.path.insert(0, '..')
-from zeroinstall.injector import gpg
+from zeroinstall.injector import gpg, model
 
 err_sig = """-----BEGIN PGP MESSAGE-----
 Version: GnuPG v1.4.0 (GNU/Linux)
@@ -40,7 +40,56 @@ GRVbJusevCKvtoSn7RAW2mg=
 -----END PGP SIGNATURE-----
 """
 
+thomas_key = """-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: GnuPG v1.0.7 (GNU/Linux)
+
+mQGiBD1JRcERBADIOjwNaBjmv44a3DPJeVwqrdVO6nuYF16UwKXTAh3ZZNAYecD8
+a7opNf4yt3TofSKfT2bEiv/hIdAy3LGjKQg54Dou1EqhB8o90RNl5NeWmHIb82Jp
+bCSbAXfaEaz6MEIg0MTHBcvtAOHZbKoBuBO5b6nbokmvcyWZXJHQ9zs9dwCg4FSX
+cdVBExg+2iBzEzpGyK4EFrsEAKTxf2YoLGihB1HDknvlAWIfa5dBZI9c7pdbpmkW
+6nZZ+SEHC9j1VSWFbB1fpA217BPaF6bmKmLoZEdmYLItriy2GEeEnbAcqd9QvQTr
+RnXzBlOanC4OHqT0dvBLMH60TsWN2ZQQ3hPInI+CAdgquDzqoZY699moo+NXZZky
+bB12A/9aI83jzl8gX7j61hkdk97rL/tcrdp8nGe2mS7y6tLodh89kp0IAD3Cn9pu
+bQpEVMSIAO6ocMIMa6IhiSW+axKcW44JaOXtxFhLi9RDnGhds9LKPSB+Qoyfpxkk
+zcAjNFcR2tDMOaDD5+/cZHSfKhT6TuWiiAzhhZEw3ikBnhCQYLQtVGhvbWFzIExl
+b25hcmQgPHRhbDE5N0B1c2Vycy5zb3VyY2Vmb3JnZS5uZXQ+iFkEExECABkFAj1J
+RcEECwcDAgMVAgMDFgIBAh4BAheAAAoJEK4HgoBZpTzBvdUAoMYjTfjeiOLyBF+V
+6tm/8Da/VIS2AKDXlYeko8yY/DMZDy9uLrmlrOLYmrkBDQQ9SUXGEAQA40HXju3P
+alvuv73gX0PcNC1lVTE3X15DTdvQLCCCt0H62A73i22c80CfGj3LaVybOHPjuM2/
+phu69zf5S3wHFJXYzezkVO7Yf/0MRyQslviy/+pWdbBJnVaE+qF3wggvcHIddatd
+roJ7q1haFl+cmIf43+EqoDZWVtKejSyeuGsAAwUEAOIrD9sPoing4huSDDgNJ9bo
+DbG3YkT9GROZ2FMdz12pwjUvSSxa8Yh4zJQ1EkKprSCD7QZMu9FMudzuwHZweJN1
+OhG+amFSsHmYl4Cbql9401lZvpvWoBhi54eKGMaxDNIGyojWJD8FTiC2eUrMwu3G
+rXu8m0nbaNiXL88Kv6EHiEYEGBECAAYFAj1JRcYACgkQrgeCgFmlPMHF8ACfehcT
+YkxNRG4ozQP5gwBO8CDdGVAAn0P7xyghEym4gcy7/rvwkY7JIar5
+=wks3
+-----END PGP PUBLIC KEY BLOCK-----
+"""
+
+gnupg_home = tempfile.mktemp()
+os.environ['GNUPGHOME'] = gnupg_home
+
 class TestGPG(unittest.TestCase):
+	def setUp(self):
+		os.mkdir(gnupg_home, 0700)
+		stream = tempfile.TemporaryFile()
+		stream.write(thomas_key)
+		stream.seek(0)
+		gpg.import_key(stream)
+	
+	def tearDown(self):
+		shutil.rmtree(gnupg_home)
+	
+	def testImportBad(self):
+		stream = tempfile.TemporaryFile()
+		stream.write("Bad key")
+		stream.seek(0)
+		try:
+			gpg.import_key(stream)
+			assert False
+		except model.SafeException:
+			pass	# OK
+
 	def testErrSig(self):
 		stream = tempfile.TemporaryFile()
 		stream.write(err_sig)
@@ -52,6 +101,7 @@ class TestGPG(unittest.TestCase):
 		assert sigs[0].need_key() == "8C6289C86DBDA68E"
 		self.assertEquals("17", sigs[0].status[gpg.ErrSig.ALG])
 		assert sigs[0].is_trusted() is False
+		assert str(sigs[0]).startswith('ERROR')
 
 	def testBadSig(self):
 		stream = tempfile.TemporaryFile()
@@ -64,6 +114,8 @@ class TestGPG(unittest.TestCase):
 		self.assertEquals("AE07828059A53CC1",
 				  sigs[0].status[gpg.BadSig.KEYID])
 		assert sigs[0].is_trusted() is False
+		assert sigs[0].need_key() is None
+		assert str(sigs[0]).startswith('BAD')
 
 	def testGoodSig(self):
 		stream = tempfile.TemporaryFile()
@@ -76,6 +128,18 @@ class TestGPG(unittest.TestCase):
 		self.assertEquals("92429807C9853C0744A68B9AAE07828059A53CC1",
 				  sigs[0].status[gpg.ValidSig.FINGERPRINT])
 		assert sigs[0].is_trusted() is True
+		assert sigs[0].need_key() is None
+		assert str(sigs[0]).startswith('Valid')
+	
+	def testNoSig(self):
+		stream = tempfile.TemporaryFile()
+		stream.write("Hello")
+		stream.seek(0)
+		try:
+			gpg.check_stream(stream)
+			assert False
+		except model.SafeException:
+			pass	# OK
 
 suite = unittest.makeSuite(TestGPG)
 if __name__ == '__main__':
