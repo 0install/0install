6734db1ccdba2deedf934e4650fc127f78ae322b
Thomas Leonard <talex5@gmail.com>
2005-09-17 13:09:24 +0000

    Added testdownload unit tests, which test the download code by forking
    an HTTP server.
    
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/injector/injector@531 9f8c893c-44ee-0310-b757-c8ca8341c71e

---
+++ tests/testdownload.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2.3
+import sys, tempfile, os, shutil
+from StringIO import StringIO
+import unittest
+from logging import getLogger, DEBUG, INFO
+#getLogger().setLevel(DEBUG)
+
+sys.path.insert(0, '..')
+
+config_home = tempfile.mktemp()
+cache_home = tempfile.mktemp()
+os.environ['XDG_CONFIG_HOME'] = config_home
+os.environ['XDG_CACHE_HOME'] = cache_home
+
+from zeroinstall.injector import model, basedir, autopolicy, gpg, iface_cache
+import data
+reload(basedir)
+
+import server
+
+foo_iface_uri = 'http://foo'
+
+class No:
+	def readline(self):
+		return "n\n"
+
+class TestDownload(unittest.TestCase):
+	def setUp(self):
+		os.mkdir(config_home, 0700)
+		os.mkdir(cache_home, 0700)
+		if os.environ.has_key('DISPLAY'):
+			del os.environ['DISPLAY']
+		self.gnupg_home = tempfile.mktemp()
+		os.environ['GNUPGHOME'] = self.gnupg_home
+		os.mkdir(self.gnupg_home, 0700)
+		stream = tempfile.TemporaryFile()
+		stream.write(data.thomas_key)
+		stream.seek(0)
+		gpg.import_key(stream)
+		iface_cache.iface_cache._interfaces = {}
+	
+	def tearDown(self):
+		shutil.rmtree(config_home)
+		shutil.rmtree(cache_home)
+		shutil.rmtree(self.gnupg_home)
+	
+	def testGetIFace(self):
+		old_out = sys.stdout
+		try:
+			sys.stdout = StringIO()
+			child = server.handle_requests('Hello', '6FCF121BE2390E0B.gpg')
+			policy = autopolicy.AutoPolicy('http://localhost:8000/Hello', download_only = False)
+			assert policy.need_download()
+			sys.stdin = No()
+			try:
+				policy.download_and_execute(['Hello'])
+				assert 0
+			except model.SafeException, ex:
+				assert "Not signed with a trusted key" in str(ex)
+			os.waitpid(child, 0)
+		finally:
+			sys.stdout = old_out
+
+suite = unittest.makeSuite(TestDownload)
+if __name__ == '__main__':
+	sys.argv.append('-v')
+	unittest.main()
