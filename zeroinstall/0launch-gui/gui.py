50bfdd2b8fb1594d174e8ee715b4e0de4862bc7c
Thomas Leonard <talex5@gmail.com>
2006-10-22 12:11:10 +0000

    New release.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0launch@1140 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- zeroinstall/0launch-gui/gui.py
+++ zeroinstall/0launch-gui/gui.py
@@ -7,7 +7,7 @@ from zeroinstall.injector.reader import InvalidInterface
 import dialog
 from checking import CheckingBox
 
-version = '0.23.1'
+version = '0.23.2'
 
 # Singleton Policy
 policy = None
