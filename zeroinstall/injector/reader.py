3f64ffdd8ac60950f126d504d674124b3731c3f9
Thomas Leonard <talex5@gmail.com>
2007-01-09 18:59:17 +0000

    Fixed bug in error reporting of unknown stability level names.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0launch@1359 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- zeroinstall/injector/reader.py
+++ zeroinstall/injector/reader.py
@@ -299,10 +299,10 @@ def update(interface, source, local = False):
 		try:
 			stability = stability_levels[str(item_attrs['stability'])]
 		except KeyError:
-			stab = str(item_attrs.stability)
+			stab = str(item_attrs['stability'])
 			if stab != stab.lower():
 				raise InvalidInterface('Stability "%s" invalid - use lower case!' % item_attrs.stability)
-			raise InvalidInterface('Stability "%s" invalid' % item_attrs.stability)
+			raise InvalidInterface('Stability "%s" invalid' % item_attrs['stability'])
 		if stability >= preferred:
 			raise InvalidInterface("Upstream can't set stability to preferred!")
 		impl.upstream_stability = stability
