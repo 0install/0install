498d049c732e0043a3789efbf8e8dddb616cc5b9
Thomas Leonard <talex5@gmail.com>
2007-02-25 10:47:39 +0000

    New release.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0launch@1553 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- zeroinstall/__init__.py
+++ zeroinstall/__init__.py
@@ -10,7 +10,7 @@ The Python implementation of the Zero Install injector is divided into three pac
 @see: U{http://0install.net}
 """
 
-version = '0.25'
+version = '0.26'
 
 class SafeException(Exception):
 	"""An exception that can be reported to the user without a stack trace.
