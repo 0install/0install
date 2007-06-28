957c68077efdfc78b2b069ee5ae93482aa1b78ad
Thomas Leonard <talex5@gmail.com>
2007-06-28 15:35:31 +0000

    Remove new-line from tar version (for nicer debug output).
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0launch@1924 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- zeroinstall/zerostore/unpack.py
+++ zeroinstall/zerostore/unpack.py
@@ -30,7 +30,7 @@ _tar_version = None
 def _get_tar_version():
 	global _tar_version
 	if _tar_version is None:
-		_tar_version = os.popen('tar --version 2>&1').next()
+		_tar_version = os.popen('tar --version 2>&1').next().strip()
 		debug("tar version = %s", _tar_version)
 	return _tar_version
 
