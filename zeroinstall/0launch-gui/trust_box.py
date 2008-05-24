29681123844e769d0b1fa2638f10c9fd10c0f48a
Thomas Leonard <talex5@gmail.com>
2008-05-24 17:36:05 +0100

    Moved coding line nearer to the top.
    Python needs it to be in the first two lines.

--- zeroinstall/0launch-gui/trust_box.py
+++ zeroinstall/0launch-gui/trust_box.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2008, Thomas Leonard
-# See the README file for details, or visit http://0install.net.
 # -*- coding: utf-8 -*-
+# See the README file for details, or visit http://0install.net.
 
 import gtk
 from zeroinstall.injector.model import SafeException
