db71bb1995887676a7fb62cf30da56165a609a44
Thomas Leonard <talex5@gmail.com>
2008-06-16 16:34:47 +0100

    Gave Zero Install its own submenu in Applications.
    Instead of having 'Add Zero Install Program' on the Accessories menu,
    we now have a top-level "Zero Install" menu with "Add Program" and
    "Update or Remove Programs" items (which currently just opens the
    cache viewer).

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
+from distutils.util import convert_path
 from distutils.command.build_py import build_py
+from distutils.command.install import install
 from distutils.command.install_lib import install_lib
+from distutils.command.install_data import install_data
 import os
 import zeroinstall
 
@@ -25,11 +28,31 @@ class build_with_data(build_py):
 
 class install_lib_exec(install_lib):
 	def run(self):
-		install_lib.run(self)
+		install_lib.run(self)	# super.run()
 		if os.name != 'posix': return
 
 		launch = os.path.join(self.install_dir, 'zeroinstall/0launch-gui/0launch-gui')
 		os.chmod(launch, os.stat(launch).st_mode | 0111)
+
+# distutils doesn't seem to have any support for adding configuration files.
+# Unfortunately, the freedesktop.org menu spec strangely defines part of the
+# menu definitions as configuration.
+class my_install(install):
+	def finalize_options(self):
+		install.finalize_options(self)	# super.finalize_options()
+		if self.home:
+			self.__config_dir = os.path.join(self.home, '.config')
+		elif self.prefix == '/usr':
+			self.__config_dir = os.path.join(self.root or '/', 'etc/xdg')
+		else:
+			self.__config_dir = os.path.join(self.prefix, 'etc/xdg')
+
+	def run(self):
+		install.run(self)	# super.run()
+		menus_dir = os.path.join(self.__config_dir, 'menus/applications-merged')
+		self.mkpath(menus_dir)
+		menu = convert_path('applications/zeroinstall.menu')
+		self.copy_file(menu, menus_dir)
 
 setup(name="zeroinstall-injector",
       version=zeroinstall.version,
@@ -39,12 +62,14 @@ setup(name="zeroinstall-injector",
       url="http://0install.net",
       scripts=['0launch', '0alias', '0store', '0store-secure-add'],
       data_files = [('man/man1', ['0launch.1', '0alias.1', '0store-secure-add.1', '0store.1']),
-      		    ('share/applications', ['applications/zeroinstall-zero2desktop.desktop']),
-      		    ('share/pixmaps', ['applications/zeroinstall-zero2desktop.png'])],
+		    ('share/applications', ['applications/zeroinstall-zero2desktop.desktop', 'applications/zeroinstall-manage.desktop']),
+		    ('share/desktop-directories', ['applications/zeroinstall.directory']),
+		    ('share/pixmaps', ['applications/zeroinstall-zero2desktop.png'])],
       license='LGPL',
       cmdclass={
-      	'build_py': build_with_data,
-      	'install_lib': install_lib_exec,
+	'build_py': build_with_data,
+	'install_lib': install_lib_exec,
+	'install': my_install,
       },
       long_description="""\
 A running process is created by combining many different libraries (and other
