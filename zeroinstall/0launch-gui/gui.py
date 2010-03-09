674af589f6be61b9f96da1e0763a4e9c044d6ddc
Thomas Leonard <talex5@gmail.com>
2010-03-09 20:11:26 +0000

    Release 0.45

--- zeroinstall/0launch-gui/gui.py
+++ zeroinstall/0launch-gui/gui.py
@@ -6,7 +6,7 @@ import gobject
 from zeroinstall.support import tasks
 from zeroinstall.injector import handler, download
 
-version = '0.44'
+version = '0.45'
 
 class GUIHandler(handler.Handler):
 	dl_callbacks = None		# Download -> [ callback ]
