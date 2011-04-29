deaa0001747908e982b93c1f49c46ae937f45360
Thomas Leonard <talex5@gmail.com>
2011-04-29 09:57:51 +0100

    Release 1.0-rc1

--- zeroinstall/0launch-gui/gui.py
+++ zeroinstall/0launch-gui/gui.py
@@ -7,7 +7,7 @@ from zeroinstall import _
 from zeroinstall.support import tasks
 from zeroinstall.injector import handler, download
 
-version = '0.54'
+version = '1.0-rc1'
 
 class GUIHandler(handler.Handler):
 	dl_callbacks = None		# Download -> [ callback ]
