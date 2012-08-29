0install/0install

commit d856e5e567f9285b5c8039d99154a3fb564c31fd

Batch up queries to PackageKit
PackageKit (on Debian at least) takes about 0.7s per request, regardless of how
many packages you ask about. This patch reduces the time taken to get the
necessary information about the Subversion feed from about 6.7s to 0.7s.

zeroinstall/injector/packagekit.py
@@ -29,6 +29,10 @@ class PackageKit(object):
 
 		self._candidates = {}	# { package_name : (version, arch, size) | Blocker }
 
+		# PackageKit is really slow at handling separate queries, so we use this to
+		# batch them up.
+		self._next_batch = set()
+
 	@property
 	def available(self):
 		return self.pk is not None
@@ -84,6 +88,14 @@ class PackageKit(object):
 	@tasks.async
 	def fetch_candidates(self, package_names):
 		assert self.pk
+
+		# Batch requests up
+		self._next_batch |= set(package_names)
+		yield
+		package_names = self._next_batch
+		self._next_batch = set()
+		# The first fetch_candidates instance will now have all the packages
+		# For the others, package_names will now be empty
 
 		known = [self._candidates[p] for p in package_names if p in self._candidates]
 		# (use set because a single task may be checking multiple packages and we need

commit 1f944edbe3533a93b3a532fbbe1fa4a2126250fa

Fixed batching of PackageKit requests
This meant we would sometimes fail to wait for the PackageKit results. Was
broken by d856e5e56.

zeroinstall/injector/packagekit.py
@@ -14,6 +14,7 @@ from zeroinstall.support import tasks, unicode
 from zeroinstall.injector import download, model
 
 _logger_pk = logging.getLogger('0install.packagekit')
+#_logger_pk.setLevel(logging.DEBUG)
 
 try:
 	import dbus
@@ -96,19 +97,32 @@ class PackageKit(object):
 		# Batch requests up
 		self._next_batch |= set(package_names)
 		yield
-		package_names = self._next_batch
+		batched_package_names = self._next_batch
 		self._next_batch = set()
-		# The first fetch_candidates instance will now have all the packages
-		# For the others, package_names will now be empty
+		# The first fetch_candidates instance will now have all the packages.
+		# For the others, batched_package_names will now be empty.
+		# Fetch any we're missing.
+		self._fetch_batch(list(batched_package_names))
 
-		known = [self._candidates[p] for p in package_names if p in self._candidates]
-		# (use set because a single task may be checking multiple packages and we need
-		# to avoid duplicates).
-		in_progress = list(set([b for b in known if isinstance(b, tasks.Blocker)]))
-		_logger_pk.debug('Already downloading: %s', in_progress)
+		results = [self._candidates[p] for p in package_names]
 
-		# Filter out the ones we've already fetched
-		package_names = [p for p in package_names if p not in self._candidates]
+		# (use set because a single Blocker may be checking multiple
+		# packages and we need to avoid duplicates).
+		in_progress = list(set([b for b in results if isinstance(b, tasks.Blocker)]))
+		_logger_pk.debug('Currently querying PackageKit for: %s', in_progress)
+
+		while in_progress:
+			yield in_progress
+			in_progress = [b for b in in_progress if not b.happened]
+
+	def _fetch_batch(self, package_names):
+		"""Ensure that each of these packages is in self._candidates.
+		Start a new fetch if necessary. Ignore packages that are already downloaded or
+		in the process of being downloaded.
+		"""
+		# (do we need a 'force' argument here?)
+
+		package_names = [n for n in package_names if n not in self._candidates]
 
 		def do_batch(package_names):
 			#_logger_pk.info("sending %d packages in batch", len(package_names))
@@ -170,8 +184,6 @@ class PackageKit(object):
 				_logger_pk.debug(_('Ask for %s'), package_names)
 				tran = _PackageKitTransaction(self.pk, resolve_cb, error_cb)
 				tran.proxy.Resolve('none', package_names)
-
-				in_progress.append(blocker)
 			except:
 				__, ex, tb = sys.exc_info()
 				blocker.trigger((ex, tb))
@@ -184,10 +196,6 @@ class PackageKit(object):
 			next_batch = package_names[:MAX_PACKAGE_KIT_TRANSACTION_SIZE]
 			package_names = package_names[MAX_PACKAGE_KIT_TRANSACTION_SIZE:]
 			do_batch(next_batch)
-
-		while in_progress:
-			yield in_progress
-			in_progress = [b for b in in_progress if not b.happened]
 
 class PackageKitDownload:
 	def __init__(self, url, hint, pk, packagekit_id, expected_size):

tests/testpackagekit.py
@@ -145,10 +145,14 @@ class TestPackageKit(BaseTest):
 		pk.get_candidates('gimp', factory, 'package:null')
 
 	def testPackageKit05(self):
-		#import logging; logging.getLogger().setLevel(logging.DEBUG)
+		#import logging
+		#_logger_pk = logging.getLogger('0install.packagekit')
+		#_logger_pk.setLevel(logging.DEBUG)
+
+		pk05 = PackageKit05()
 
 		dbus.system_services['org.freedesktop.PackageKit'] = {
-			'/org/freedesktop/PackageKit': PackageKit05(),
+			'/org/freedesktop/PackageKit': pk05,
 			'/tid/1': PackageKit05.Tid1(),
 			'/tid/2': PackageKit05.Tid2(),
 			'/tid/3': PackageKit05.Install(),
@@ -157,12 +161,21 @@ class TestPackageKit(BaseTest):
 		pk = packagekit.PackageKit()
 		assert pk.available
 
+		# Check none is found yet
 		factory = Exception("not called")
 		pk.get_candidates('gimp', factory, 'package:test')
 
 		blocker = pk.fetch_candidates(["gimp"])
-		tasks.wait_for_blocker(blocker)
-		tasks.check(blocker)
+		blocker2 = pk.fetch_candidates(["gimp"])		# Check batching too
+
+		@tasks.async
+		def wait():
+			yield blocker, blocker2
+			if blocker.happened:
+				tasks.check(blocker)
+			else:
+				tasks.check(blocker2)
+		tasks.wait_for_blocker(wait())
 
 		impls = {}
 		def factory(impl_id, only_if_missing, installed):
@@ -188,6 +201,13 @@ class TestPackageKit(BaseTest):
 		tasks.wait_for_blocker(b)
 		tasks.check(b)
 		self.assertEqual("/usr/bin/fixed", list(impls.values())[0].main)
+
+		tasks.wait_for_blocker(blocker)
+		tasks.wait_for_blocker(blocker2)
+
+		# Don't fetch it again
+		tasks.wait_for_blocker(pk.fetch_candidates(["gimp"]))
+		self.assertEqual(3, pk05.x)
 	
 	def installed_fixup(self, impl):
 		impl.main = '/usr/bin/fixed'