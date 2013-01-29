0install/0install

commit a99b0fad2d1117785d6d38ca010b612f2960260e

Better error if an invalid interface is a valid alias
Now suggests using the alias: prefix.

zeroinstall/injector/model.py
@@ -1447,6 +1447,19 @@ def canonical_iface_uri(uri):
 
 	if os.path.isfile(path):
 		return path
+
+	if '/' not in uri:
+		path = support.find_in_path(uri)
+		if path is not None:
+			from zeroinstall import alias
+			try:
+				alias.parse_script(path)
+			except alias.NotAnAliasScript:
+				pass
+			else:
+				raise SafeException(_("Bad interface name '{uri}'.\n"
+					"(hint: try 'alias:{uri}' instead)".format(uri = uri)))
+
 	raise SafeException(_("Bad interface name '%(uri)s'.\n"
 			"(doesn't start with 'http:', and "
 			"doesn't exist as a local file '%(interface_uri)s' either)") %

tests/testinstall.py
@@ -479,6 +479,14 @@ class TestInstall(BaseTest):
 			else:
 				self.assertEqual(expected, ex.man_args)
 
+	def testAlias(self):
+		alias_path = os.path.join(mydir, '..', '0alias')
+		local_feed = os.path.join(mydir, 'Local.xml')
+		subprocess.check_call([alias_path, 'my-test-alias', local_feed])
+		out, err = self.run_0install(['update', 'my-test-alias'])
+		self.assertEqual("Bad interface name 'my-test-alias'.\n(hint: try 'alias:my-test-alias' instead)\n", err)
+		self.assertEqual("", out)
+
 	def testMan(self):
 		out, err = self.run_0install(['man', '--help'])
 		assert out.lower().startswith("usage:")

commit 538e1a323ca8d4d8b223db6d4925bf265b208d3f

Fixed error message when an interface doesn't exist
Was broken by a99b0fad2d111. Reported by Tim Cuthbertson.

zeroinstall/injector/model.py
@@ -1449,11 +1449,11 @@ def canonical_iface_uri(uri):
 		return path
 
 	if '/' not in uri:
-		path = support.find_in_path(uri)
-		if path is not None:
+		alias_path = support.find_in_path(uri)
+		if alias_path is not None:
 			from zeroinstall import alias
 			try:
-				alias.parse_script(path)
+				alias.parse_script(alias_path)
 			except alias.NotAnAliasScript:
 				pass
 			else:

tests/testmodel.py
@@ -252,6 +252,7 @@ class TestModel(BaseTest):
 			assert False
 		except model.SafeException as ex:
 			assert "Bad interface name 'CommandMissing.xml'" in str(ex), ex
+			assert "/tests/CommandMissing.xml' either" in str(ex), ex
 
 		# file:absolute
 		model.canonical_iface_uri('file://{path}/Command.xml'.format(path = mydir))