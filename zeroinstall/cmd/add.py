60ec67d467f93d99c46b74bfb27a6ee63d20f0a9
Thomas Leonard <talex5@gmail.com>
2013-03-11 10:02:21 +0000

    "0install add" now prints a warning if the target has a replaced-by field

--- zeroinstall/cmd/add.py
+++ zeroinstall/cmd/add.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 import sys
 
+from zeroinstall import _
 from zeroinstall.cmd import UsageError, select
 from zeroinstall.injector import model, requirements
 
@@ -27,6 +28,12 @@ def handle(config, options, args):
 	sels = select.get_selections(config, options, iface_uri, select_only = False, download_only = True, test_callback = None)
 	if not sels:
 		sys.exit(1)	# Aborted by user
+
+	root_feed = config.iface_cache.get_feed(iface_uri)
+	if root_feed:
+		target = root_feed.get_replaced_by()
+		if target is not None:
+			print(_("Warning: interface {old} has been replaced by {new}".format(old = iface_uri, new = target)))
 
 	r = requirements.Requirements(iface_uri)
 	r.parse_options(options)
