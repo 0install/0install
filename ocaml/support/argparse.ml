0install/0install

commit 76e27bff90888aa06fd1654a3e67a4a081a8a49b

List.iteri is only available in OCaml >= 4

ocaml/support/argparse.ml
@@ -293,8 +293,13 @@ let format_options format_type opts =
   let set_tab_stop = ref false in
   set_tab();
 
-  ListLabels.iteri display_options ~f:(fun opti (syn, help) ->
-    if opti > 0 then print_cut ();
+  let first = ref true in
+
+  ListLabels.iter display_options ~f:(fun (syn, help) ->
+    if !first then (
+      print_cut ();
+      first := false
+    );
 
     print_string syn;
 

commit 9ad38b0effbc652a01e4f64b9539ce08c7f9164c

Fixed formatting of help output
Was broken by 76e27bff.

ocaml/support/argparse.ml
@@ -297,8 +297,9 @@ let format_options format_type opts =
 
   ListLabels.iter display_options ~f:(fun (syn, help) ->
     if !first then (
-      print_cut ();
       first := false
+    ) else (
+      print_cut ()
     );
 
     print_string syn;