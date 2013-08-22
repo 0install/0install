def66bc381f03970640a61d64b49ad5de9ef3879
Thomas Leonard <talex5@gmail.com>
2013-08-22 15:40:43 +0100

    Remove OCAMLLIB from build environment
    This is a temporary hack: when we can depend on a full OCaml feed with
    the build tools, we can remove this. Until then, we need to avoid trying
    to compile against the limited runtime environment.

--- ocaml/build-in.py
+++ ocaml/build-in.py
@@ -3,4 +3,10 @@ import sys
 import os
 from os.path import relpath
 ocaml_build_dir = relpath(sys.argv[1], '.')
+
+# Hack: when we can depend on a full OCaml feed with the build tools, we can remove this.
+# Until then, we need to avoid trying to compile against the limited runtime environment.
+if 'OCAMLLIB' in os.environ:
+	del os.environ['OCAMLLIB']
+
 os.execvp("make", ["make", 'OCAML_BUILDDIR=' + ocaml_build_dir, "ocaml"])
