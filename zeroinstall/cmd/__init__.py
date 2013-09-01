7773c0283f13135ac32ed6225658557faaa71a3d
Thomas Leonard <talex5@gmail.com>
2013-09-01 11:09:21 +0100

    Removed unused Python code

--- zeroinstall/cmd/__init__.py
+++ zeroinstall/cmd/__init__.py
@@ -14,7 +14,7 @@ import logging
 
 from zeroinstall import SafeException, DryRun
 
-valid_commands = ['add', 'select', 'download', 'run', 'update', 'whatchanged', 'destroy',
+valid_commands = ['add', 'select', 'whatchanged', 'destroy',
 		  'config', 'import', 'list', 'search', 'add-feed', 'remove-feed', 'list-feeds',
 		  'digest', 'slave']
 
