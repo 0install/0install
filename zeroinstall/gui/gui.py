e664b5ee741833eaace0aaed54e4a2d0d3f1589e
Thomas Leonard <talex5@gmail.com>
2013-09-17 17:28:42 +0100

    Start development series 2.4-rc1-post

--- zeroinstall/gui/gui.py
+++ zeroinstall/gui/gui.py
@@ -8,7 +8,7 @@ from zeroinstall.injector import handler, download
 gobject = tasks.get_loop().gobject
 glib = tasks.get_loop().glib
 
-version = '2.4-rc1'
+version = '2.4-rc1-post'
 
 class GUIHandler(handler.Handler):
 	pulse = None
