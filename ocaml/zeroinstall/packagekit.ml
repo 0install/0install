d221dfc52de65b095267771919d885fd3143f9d4
Thomas Leonard <talex5@gmail.com>
2016-05-02 11:36:19 +0100

    Remove Lwt syntax extension from Packagekit

--- ocaml/zeroinstall/packagekit.ml
+++ ocaml/zeroinstall/packagekit.ml
@@ -68,18 +68,19 @@ let add_package details ~package_id ~summary =
 (** Convert short names to PackageKit IDs (e.g. "gnupg" -> "gnupg;2.0.22;x86_64;arch") *)
 let resolve proxy package_names =
   let details = Hashtbl.create 10 in
-  lwt () =
-    try_lwt
-      Packagekit_stubs.summaries proxy ~package_names (add_package details)
-    with Safe_exception _ as ex ->
-      (* This is a bit broken. PackageKit seems to abort on the first unknown package, so we
-       * lose the remaining results. Still, something is better than nothing... *)
-      log_debug ~ex "Error resolving %s with PackageKit" (String.concat "," package_names);
-      Lwt.return () in
-
+  Lwt.catch
+    (fun () -> Packagekit_stubs.summaries proxy ~package_names (add_package details))
+    (function
+      | Safe_exception _ as ex ->
+        (* This is a bit broken. PackageKit seems to abort on the first unknown package, so we
+         * lose the remaining results. Still, something is better than nothing... *)
+        log_debug ~ex "Error resolving %s with PackageKit" (String.concat "," package_names);
+        Lwt.return ()
+      | ex -> Lwt.fail ex
+    )
+  >|= fun () ->
   let add map name = map |> StringMap.add name (Hashtbl.find_all details name) in
-  let results = package_names |> List.fold_left add StringMap.empty in
-  Lwt.return results
+  package_names |> List.fold_left add StringMap.empty
 
 (** Get the sizes of the candidate packages. *)
 let get_sizes proxy = function
@@ -100,12 +101,20 @@ let rec get_total acc = function
   | (_impl, {Impl.distro_size = Some size; _}) :: xs -> get_total (Int64.add acc size) xs
   | (_impl, {Impl.distro_size = None; _}) :: _ -> None
 
+let with_finished cb =
+  let finished, set_finished = Lwt_react.S.create false in
+  Lwt.finalize
+    (fun () -> cb finished)
+    (fun () ->
+       set_finished true;
+       Lwt.return ()
+    )
+
 (** Install distribution packages. *)
 let install (ui:#ui) proxy items =
   let packagekit_ids = items |> List.map (fun (_impl, rm) -> get_packagekit_id rm) in
   let total_size = get_total Int64.zero items in
-  let finished, set_finished = Lwt_react.S.create false in
-  try_lwt
+  with_finished @@ fun finished ->
     let cancelled = ref false in
     Packagekit_stubs.run_transaction proxy (fun switch proxy ->
       Packagekit_stubs.Transaction.monitor ~switch proxy >>= fun percentage ->
@@ -120,13 +129,13 @@ let install (ui:#ui) proxy items =
         if not !cancelled then (
           cancelled := true;
           log_info "Cancelling PackageKit installation";
-          try_lwt
-            Packagekit_stubs.Transaction.cancel proxy
-          with ex ->
-            log_warning ~ex "packagekit: cancel failed";
-            Lwt.return ()
+          Lwt.catch
+            (fun () -> Packagekit_stubs.Transaction.cancel proxy)
+            (fun ex ->
+               log_warning ~ex "packagekit: cancel failed";
+               Lwt.return ()
+            )
         ) else Lwt.return () in
-
       (* Notify the start of all downloads (we share the overall progress between them currently). *)
       items |> List.iter (fun (impl, _rm) ->
         let main_feed =
@@ -135,7 +144,6 @@ let install (ui:#ui) proxy items =
           | (`local_feed x | `remote_feed x) as main_feed -> log_warning "Not a distribution feed: %s" x; main_feed in
         ui#monitor Downloader.({cancel; url = "(packagekit)"; progress; hint = Some (Feed_url.format_url main_feed)})
       );
-
       Packagekit_stubs.Transaction.install_packages proxy packagekit_ids
     ) >>= fun () ->
     (* Mark each package as now installed (possibly we should do this individually in a signal callback instead). *)
@@ -145,79 +153,110 @@ let install (ui:#ui) proxy items =
     );
     ui#impl_added_to_store;
     Lwt.return (if !cancelled then `cancel else `ok)
-  finally
-    set_finished true;
-    Lwt.return ()
+
+let fail_on_exn queries fn =
+  Lwt.catch fn
+    (fun ex ->
+      List.iter (fun (_, resolver) -> Lwt.wakeup_exn resolver ex) queries;
+      Lwt.return ()
+    )
+
+(* The list of packagekit IDs that were successfull resolved. *)
+let success_ids resolutions =
+  StringMap.fold (fun _ infos acc ->
+    infos |> List.fold_left (fun acc info ->
+      match info with
+      | `Ok info -> get_packagekit_id info.retrieval_method :: acc
+      | `Error _ -> acc
+    ) acc
+  ) resolutions []
+
+(* Resolve a query with its results and problems. *)
+let resolve_query ~resolutions ~sizes (name, resolver) =
+  let impls = StringMap.find name resolutions |> default [] in
+  let problems = ref [] in
+  if impls = [] then
+    problems := Printf.sprintf "'%s' details not in PackageKit response" name :: !problems;
+  (* Update retrieval method with the size information *)
+  let add_size = function
+    | `Error msg -> problems := Printf.sprintf "%s: %s" name msg :: !problems; None
+    | `Ok impl ->
+    let rm = impl.retrieval_method in
+    let packagekit_id = get_packagekit_id rm in
+    match StringMap.find packagekit_id sizes with
+    | Some _ as size -> Some {impl with retrieval_method = {rm with Impl.distro_size = size}}
+    | None ->
+        log_info "No size returned for '%s'" packagekit_id;
+        Some impl in
+  let results = U.filter_map add_size impls in
+  Lwt.wakeup resolver {
+    results;
+    problems = !problems;
+  }
+
+(* Send a single PackageKit transaction for these packages. *)
+let fetch_batch proxy queries =
+  log_info "packagekit: requesting details for %s" (String.concat ", " (List.map fst queries));
+  fail_on_exn queries @@ fun () ->
+  (* Convert short names to PackageKit IDs *)
+  resolve proxy (List.map fst queries) >>= fun resolutions ->
+  let package_ids = success_ids resolutions in
+  if List.length package_ids > 0 then
+    log_debug "packagekit: requesting package sizes for %s" (String.concat ", " (List.map String.escaped package_ids))
+  else
+    log_debug "packagekit: no packages found";
+  (* Collect package sizes *)
+  Lwt.catch
+    (fun () -> get_sizes proxy package_ids)
+    (fun ex ->
+       log_warning ~ex "packagekit: GetDetails failed";
+       Lwt.return StringMap.empty
+    )
+  >|= fun sizes ->
+  log_info "packagekit: fetch_batch done";
+  List.iter (resolve_query ~resolutions ~sizes) queries
+
+module Candidates : sig
+  type t
+  val make : unit -> t
+  val get : t -> string -> query_result Lwt.t option
+  val ensure_fetching : t -> string -> [`In_progress of query_result Lwt.t | `New_task of query_result Lwt.t * query_result Lwt.u]
+end = struct
+  type t = (string, query_result Lwt.t) Hashtbl.t
+
+  let make () = Hashtbl.create 10
+
+  let get t package_name =
+    try Some (Hashtbl.find t package_name)
+    with Not_found -> None
+
+  let ensure_fetching t name =
+    let start () =
+      let task, waker = Lwt.task () in
+      Hashtbl.replace t name task;
+      `New_task (task, waker) in
+    match get t name with
+    | None -> start ()
+    | Some existing_task ->
+      match Lwt.state existing_task with
+      | Lwt.Return _ | Lwt.Fail _ -> start ()
+      | Lwt.Sleep -> `In_progress existing_task
+end
+
+let with_simple_progress fn =
+  let progress, set_progress = Lwt_react.S.create (Int64.zero, None, false) in
+  Lwt.finalize
+    (fun () -> fn progress)
+    (fun () ->
+       set_progress (Int64.zero, None, true);
+       Lwt.return ()
+    )
 
 let make lang_spec =
   let proxy = lazy (Packagekit_stubs.connect lang_spec) in
 
-  (* Send a single PackageKit transaction for these packages. *)
-  let fetch_batch proxy queries =
-    log_info "packagekit: requesting details for %s" (String.concat ", " (List.map fst queries));
-
-    try_lwt
-      (* Convert short names to PackageKit IDs *)
-      resolve proxy (List.map fst queries) >>= fun resolutions ->
-
-      let package_ids = StringMap.fold (fun _ infos acc ->
-        infos |> List.fold_left (fun acc info ->
-          match info with
-          | `Ok info -> get_packagekit_id info.retrieval_method :: acc
-          | `Error _ -> acc
-        ) acc
-      ) resolutions [] in
-
-      if List.length package_ids > 0 then
-        log_debug "packagekit: requesting package sizes for %s" (String.concat ", " (List.map String.escaped package_ids))
-      else
-        log_debug "packagekit: no packages found";
-
-      (* Collect package sizes *)
-      lwt sizes =
-        try_lwt
-          get_sizes proxy package_ids
-        with ex ->
-          log_warning ~ex "packagekit: GetDetails failed";
-          Lwt.return StringMap.empty in
-
-      log_info "packagekit: fetch_batch done";
-      (* Notify that each query is done *)
-      queries |> List.iter (fun (name, resolver) ->
-        let impls = StringMap.find name resolutions |> default [] in
-        let problems = ref [] in
-
-        if impls = [] then
-          problems := Printf.sprintf "'%s' details not in PackageKit response" name :: !problems;
-
-        (* Update [resolutions] with the size information *)
-        let add_size = function
-          | `Error msg -> problems := Printf.sprintf "%s: %s" name msg :: !problems; None
-          | `Ok impl ->
-          let rm = impl.retrieval_method in
-          let packagekit_id = get_packagekit_id rm in
-          match StringMap.find packagekit_id sizes with
-          | Some _ as size -> Some {impl with retrieval_method = {rm with Impl.distro_size = size}}
-          | None ->
-              log_info "No size returned for '%s'" packagekit_id;
-              Some impl in
-
-        let results = U.filter_map add_size impls in
-        Lwt.wakeup resolver {
-          results;
-          problems = !problems;
-        }
-      );
-
-      Lwt.return ()
-    with ex ->
-      queries |> List.iter (fun (_, resolver) ->
-        Lwt.wakeup_exn resolver ex
-      );
-      Lwt.return () in
-
   object (_ : packagekit)
-    val candidates : (string, query_result Lwt.t) Hashtbl.t = Hashtbl.create 10
+    val candidates = Candidates.make ()
 
     (** Names of packages we're about to issue a query for and their resolvers/wakers. *)
     val mutable next_batch = []
@@ -231,8 +270,9 @@ let make lang_spec =
         The candidates are those discovered by a previous call to [check_for_candidates].
         @param package_name the distribution's name for the package *)
     method get_impls package_name =
-      let task = try Hashtbl.find candidates package_name with Not_found -> Lwt.return { results = []; problems = [] } in
-
+      match Candidates.get candidates package_name with
+      | None -> { results = []; problems = [] }
+      | Some task ->
       match Lwt.state task with
       | Lwt.Sleep -> { results = []; problems = [] }                 (* Fetch still in progress *)
       | Lwt.Fail ex -> { results = []; problems = [Printexc.to_string ex] }     (* Fetch failed *)
@@ -243,8 +283,8 @@ let make lang_spec =
       Lazy.force proxy >>= function
       | `Unavailable _ -> Lwt.return ()
       | `Ok proxy ->
-          let progress, set_progress = Lwt_react.S.create (Int64.zero, None, false) in
-          try_lwt
+        with_simple_progress @@ fun progress ->
+          Lwt.catch (fun () ->
             let waiting_for = ref [] in
 
             let cancel () = !waiting_for |> List.iter Lwt.cancel; Lwt.return () in
@@ -263,41 +303,27 @@ let make lang_spec =
              * If we're already fetching, just take the existing promise.
              *)
             package_names |> List.iter (fun name ->
-              let in_progress =
-                try
-                  let existing_task = Hashtbl.find candidates name in
-                  match Lwt.state existing_task with
-                  | Lwt.Return _ | Lwt.Fail _ -> false
-                  | Lwt.Sleep ->
-                      waiting_for := lwt_wait_for existing_task :: !waiting_for;
-                      true
-                with Not_found -> false in
-
-              if not in_progress then (
-                let task, waker = Lwt.task () in
-                Hashtbl.replace candidates name task;
+              match Candidates.ensure_fetching candidates name with
+              | `In_progress existing_task ->
+                waiting_for := lwt_wait_for existing_task :: !waiting_for
+              | `New_task (task, waker) ->
                 waiting_for := lwt_wait_for task :: !waiting_for;
                 next_batch <- (name, waker) :: next_batch;
-
                 if List.length next_batch = max_batch_size then do_batch ()
-              )
             );
 
             (* Yield briefly, so that other packages can be added to the final batch. *)
-            lwt () = Lwt_main.yield () in
+            Lwt_main.yield () >>= fun () ->
             (* (note: next_batch might also have become empty, if someone else added our items to their batch) *)
             do_batch ();
 
             Lwt.join !waiting_for
-          with
-          | Lwt.Canceled ->
-              Lwt.return ()
-          | ex ->
+          ) (function
+            | Lwt.Canceled -> Lwt.return ()
+            | ex ->
               log_warning ~ex "Error querying PackageKit";
               Lwt.return ()
-          finally
-            set_progress (Int64.zero, None, true); (* Stop progress indicator *)
-            Lwt.return ()
+          )
 
     method install_packages (ui:#ui) items : [ `ok | `cancel ] Lwt.t =
       let packagekit_ids = items |> List.map (fun (_impl, rm) -> get_packagekit_id rm) in
