6c0f5c51bc099370a367102e48723a42cd352b3b
Thomas Leonard <talex5@gmail.com>
2019-12-13 17:52:07 +0000

    Add Travis tests for static build
    Also, test OCaml 4.09 on Linux and reduce the number of macOS builds.

--- .travis-extra-deps.sh
+++ .travis-extra-deps.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
-set -eux
-# Install OCaml and OPAM PPAs
-install_on_ubuntu () {
-  sudo apt-get install -qq time libgtk2.0-dev libcurl4-openssl-dev python-gobject-2
-}
+if [ "$STATIC_DIST" == true ]; then
+  set -eux
+  make static-test
+else
+  set -eux
 
-install_on_osx () {
-  curl -OL "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg"
-  # Disable sandboxing on OS X; it prevents the unit-tests from working.
-  cat > ~/.opamrc << EOF
-wrap-build-commands: []
-wrap-install-commands: []
-wrap-remove-commands: []
-required-tools: []
+  # Install OCaml and OPAM PPAs
+  install_on_ubuntu () {
+    sudo apt-get install -qq time libgtk2.0-dev libcurl4-openssl-dev python-gobject-2
+  }
+
+  install_on_osx () {
+    curl -OL "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg"
+    # Disable sandboxing on OS X; it prevents the unit-tests from working.
+    cat > ~/.opamrc << EOF
+  wrap-build-commands: []
+  wrap-install-commands: []
+  wrap-remove-commands: []
+  required-tools: []
 EOF
-  sudo hdiutil attach XQuartz-2.7.6.dmg
-  sudo installer -verbose -pkg /Volumes/XQuartz-2.7.6/XQuartz.pkg -target /
-  brew update &> /dev/null
-  brew unlink python	# Python 3 conflicts with Python 2's /usr/local/bin/2to3-2 file
-  brew upgrade gnupg wget
-  brew install gtk+ pygobject
-  export PKG_CONFIG_PATH=/usr/local/Library/Homebrew/os/mac/pkgconfig/10.9:/usr/lib/pkgconfig
-}
+    sudo hdiutil attach XQuartz-2.7.6.dmg
+    sudo installer -verbose -pkg /Volumes/XQuartz-2.7.6/XQuartz.pkg -target /
+    brew update &> /dev/null
+    brew unlink python	# Python 3 conflicts with Python 2's /usr/local/bin/2to3-2 file
+    brew upgrade gnupg wget
+    brew install gtk+ pygobject
+    export PKG_CONFIG_PATH=/usr/local/Library/Homebrew/os/mac/pkgconfig/10.9:/usr/lib/pkgconfig
+  }
 
-case $TRAVIS_OS_NAME in
-  linux)
-         install_on_ubuntu ;;
-  osx)
-         install_on_osx ;;
-  *) echo "Unknown OS $TRAVIS_OS_NAME";
-     exit 1 ;;
-esac
+  case $TRAVIS_OS_NAME in
+    linux)
+	   install_on_ubuntu ;;
+    osx)
+	   install_on_osx ;;
+    *) echo "Unknown OS $TRAVIS_OS_NAME";
+       exit 1 ;;
+  esac
 
-# (downloaded by Travis install step)
-bash -e ./.travis-opam.sh
+  # (downloaded by Travis install step)
+  bash -e ./.travis-opam.sh
+fi
