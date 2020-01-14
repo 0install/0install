386b6affe4c25dc3a63a80f57956c34d402077b3
Thomas Leonard <talex5@gmail.com>
2020-01-14 09:55:07 +0000

    Release 2.3.8

--- zeroinstall/0launch-gui/gui.py
+++ zeroinstall/0launch-gui/gui.py
@@ -8,7 +8,7 @@ from zeroinstall.injector import handler, download
 gobject = tasks.get_loop().gobject
 glib = tasks.get_loop().glib
 
-version = '2.3.7'
+version = '2.3.8'
 
 class GUIHandler(handler.Handler):
 	dl_callbacks = None		# Download -> [ callback ]
