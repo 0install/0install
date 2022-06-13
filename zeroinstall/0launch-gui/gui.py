48ed6e0e7f1c54eff05a3e2cf56ca807f3b47a18
Bastian Eicher <bastian@eicher.net>
2022-06-13 15:23:30 +0200

    Release 2.3.15

--- zeroinstall/0launch-gui/gui.py
+++ zeroinstall/0launch-gui/gui.py
@@ -8,7 +8,7 @@ from zeroinstall.injector import handler, download
 gobject = tasks.get_loop().gobject
 glib = tasks.get_loop().glib
 
-version = '2.3.14'
+version = '2.3.15'
 
 class GUIHandler(handler.Handler):
 	dl_callbacks = None		# Download -> [ callback ]
